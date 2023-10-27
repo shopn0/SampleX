ef7a2cba2a685f3eea5fac71ede7225ee43229f0
Shopno <shariare.shwapno@gmail.com>
2023-10-27 08:47:38 +0600

    Wrote hello to the text editor.

---
+++ Starter.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    
+    // Saying hello to users
+    printf("Hello Everyone.\n");
+
+return 0;
+}
---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    //Showing the first version
+    printf("Version 01\n");
+    printf("SampleX Limited.");
+    
+
+return 0;
+}
