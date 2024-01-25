34545b436ebc2a0d86944a4a6ff51be5217ebceb
Shahriar Shopno <shariare.shwapno@gmail.com>
2024-01-26 01:28:03 +0600

    Comparison Operators

--- CutiePy/born.py
+++ CutiePy/born.py
@@ -1,6 +1,30 @@
 # This is my 2nd entry to the lang of Py
-print("I am the King")
+# From childhool interest I entered the the world of programming with this language first in year 2018 when i was just a school student with no idea of computers much.
+# Here i am again with my static goal this time to learn and master it.
 
-# Py uses interpreter which converts each high-level program statement into machine code
+print("This text has been printed from a python program.\n 26 January, 2024")
 
+"""
+Fact:
+Py uses interpreter which converts EACH high-level program statement into machine code
+"""
+#Comparison Operators
+print("Comparison Operators:")
+s1_mark = 50
+s2_mark = 60
 
+a = s1_mark == s2_mark
+b = s2_mark != s1_mark
+
+print(a)
+print(b)
+
+#Logical Comparison Operators
+print("Logical Comparison Operators:")
+
+a= False
+b= True
+c= True
+
+q = (a and b) or ((b or c)and (b and c))
+print("q=",q)
