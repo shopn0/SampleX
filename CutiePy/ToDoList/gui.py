0ae84397067abbbb8549598d1e9ff00d41fe15e0
Shahriar Shopno <shariare.shwapno@gmail.com>
2024-05-25 00:41:39 +0600

    break

---
+++ CutiePy/ToDoList/gui.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+root=Tk()
+root.title("SampleX")
+root.geometry("500x700")
+
+root.mainloop()
--- CutiePy/temp.py
+++ CutiePy/temp.py
@@ -1,13 +1,1 @@
-def prime_number(n):
-  flag = 0
-  it = 2
-  while it <= n/2:
-    if n % it == 0:
-      flag = 1
-      break
-    it += 1
-  if flag == 0:
-    print(n, end = ' ')
-
-for i in range(2, 100):
-  prime_number(i)
+print("Hello World!")
